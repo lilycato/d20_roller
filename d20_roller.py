1fd517c7b46fac456898f193266843801a7d1d5e
lilycato <146873958+lilycato@users.noreply.github.com>
2025-02-15 14:44:37 +0800

    Update d20_roller.py
    added another table to show fuller range of AC calculations

--- d20_roller.py
+++ d20_roller.py
@@ -93,50 +93,73 @@ def open_new_window():
     as_entry=tk.Entry(popup, font = ('Arial',10,'normal'))
     as_entry.place(relx=0.2, rely=0.25, anchor='center')
     
-    prof_label=tk.Label(popup, text='Enter Prof Bonus', font = ('Arial',10,'normal'))
-    prof_label.place(relx=0.2, rely=0.3, anchor='center')
+    prof_label=tk.Label(popup, text='Set Prof Bonus', font = ('Arial',10,'normal'))
+    prof_label.place(relx=0.2, rely=0.315, anchor='center')
 
-    prof_entry=tk.Entry(popup, font = ('Arial',10,'normal'))
-    prof_entry.place(relx=0.2, rely=0.35, anchor='center')
+    prof_entry=tk.Scale(popup, from_=1, to=6, orient='horizontal')
+    prof_entry.place(relx=0.2, rely=0.4, anchor='center')
 
     
-    output = tk.Listbox(popup)
-    output.place(relx=0.7, rely=0.5, anchor='center', height=300)
-    output.configure(background='skyblue4', foreground='white', font=('Arial', 15))
+    output_chance = tk.Listbox(popup)
+    output_chance.place(relx=0.55, rely=0.4, anchor='center', height=300, width=100)
+    output_chance.configure(background='skyblue4', foreground='white', font=('Arial', 10))
+    output_chance_label=tk.Label(popup, text='Chance to hit:', font = ('Arial',10,'normal'))
+    output_chance_label.place(relx=0.55, rely=0.065, anchor='center')
+    
+    output_ac = tk.Listbox(popup)
+    output_ac.place(relx=0.8, rely=0.4, anchor='center', height=300, width=100)
+    output_ac.configure(background='skyblue4', foreground='white', font=('Arial', 10))
+    output_ac_label=tk.Label(popup, text='Monster AC:', font = ('Arial',10,'normal'))
+    output_ac_label.place(relx=0.8, rely=0.065, anchor='center')
     
     
     def to_hit():
+        output_ac.delete(0,"end")
+        output_chance.delete(0,"end")
         AC = int(AC_entry.get())
         prof_bonus=int(prof_entry.get())
         AS=int(as_entry.get())
         total_bonus=prof_bonus+AS
         if total_bonus > AC:
             total_bonus =  AC
-        P = ((21 - (AC-total_bonus))/20 ) * 100
-        prob_hit = max(min(P, 95), 5)
-        output.insert(tk.END, prob_hit)
+        for i in range(10, AC):    
+            P = ((21 - (AC-total_bonus))/20 ) * 100
+            prob_hit = max(min(P, 95), 5)
+            output_chance.insert(tk.END, prob_hit)
+            output_ac.insert(tk.END, AC)
+            AC-=1
     
     def to_hit_with_adv():
+        output_ac.delete(0,"end")
+        output_chance.delete(0,"end")
         AC = int(AC_entry.get())
         prof_bonus=int(prof_entry.get())
         AS=int(as_entry.get())
         total_bonus=prof_bonus+AS
         if total_bonus > AC:
             total_bonus = AC
-        P = (1 - (((AC-total_bonus-1)**2)/400)) * 100
-        prob_hit = max(min(P, 99.75), 10)
-        output.insert(tk.END, prob_hit)
+        for i in range(10, AC): 
+            P = (1 - (((AC-total_bonus-1)**2)/400)) * 100
+            prob_hit = max(min(P, 99.75), 10)
+            output_chance.insert(tk.END, prob_hit)
+            output_ac.insert(tk.END, AC)
+            AC-=1
         
     def to_hit_with_dis():
+        output_ac.delete(0,"end")
+        output_chance.delete(0,"end")
         AC = int(AC_entry.get())
         prof_bonus=int(prof_entry.get())
         AS=int(as_entry.get())
         total_bonus=prof_bonus+AS
         if total_bonus > AC:
-            total_bonus = AC
-        P = ((((21+total_bonus-AC)**2)/400)) * 100
-        prob_hit = max(min(P, 90.25), 0.25)
-        output.insert(tk.END, prob_hit)
+            total_bonus = AC    
+        for i in range(10, AC): 
+            P = ((((21+total_bonus-AC)**2)/400)) * 100
+            prob_hit = max(min(P, 90.25), 0.25)
+            output_chance.insert(tk.END, prob_hit)
+            output_ac.insert(tk.END, AC)
+            AC-=1
         
     button2 = tk.Button(popup, text='Calculate (No Adv)', command=to_hit)
     button2.place(relx=0.2, rely=0.5, anchor='center')
